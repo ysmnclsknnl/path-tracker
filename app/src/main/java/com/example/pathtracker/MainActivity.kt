bbe34adc56990e56caba05c79231ebc8274b40ce
Yasemin Caliskan <yasemin.caliskan@tomtom.com>
2025-11-07 15:15:05 +0100

    add request permission request to main activity
    location permissions

--- app/src/main/java/com/example/pathtracker/MainActivity.kt
+++ app/src/main/java/com/example/pathtracker/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.pathtracker
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.example.pathtracker.ui.theme.PathtrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +24,62 @@ class MainActivity : ComponentActivity() {
         setContent {
             PathtrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainScreen(
+                        name = "Path tracker",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
+        if (!areLocationPermissionsGranted()) {
+            requestLocationPermissions()
+        }
+    }
+
+    /**
+     * Method to verify permissions:
+     * - [Manifest.permission.ACCESS_FINE_LOCATION]
+     * - [Manifest.permission.ACCESS_COARSE_LOCATION]
+     */
+    private fun areLocationPermissionsGranted() = ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private val locationPermissionRequest =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions(),
+        ) { permissions ->
+            if (permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true &&
+                permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+            ) {
+                // Show user location
+            } else {
+                Toast.makeText(
+                    this,
+                    getString(R.string.location_permission_denied),
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
+        }
+
+    private fun requestLocationPermissions() {
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            ),
+        )
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MainScreen(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PathtrackerTheme {
-        Greeting("Android")
-    }
 }
